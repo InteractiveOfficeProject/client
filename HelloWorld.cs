f3b707ebef94cfa45ec77fd256ceab48eae26639
Jan Lippert <lippertsjan@gmail.com>
2017-04-06 21:48:01 +0300

    Initial project

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿namespace InteractiveOfficeClient
+{
+    using Gtk;
+    public class HelloWorld {
+        static void Main() {
+            Application.Init ();
+            MessageDialog Hello = new MessageDialog (null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Hello world!");
+            Hello.SecondaryText="This is an example dialog.";
+            Hello.Run ();
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # client
 Desktop-Client for our DIS project
+
+
+## Setup
+
+Install [gtksharp](http://www.mono-project.com/docs/gui/gtksharp/).
+
+On manjaro/arch this is contained in the package `gtk-sharp-3`. For 
+MacOs and Windows, see http://www.mono-project.com/download/
+
+
