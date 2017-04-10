87c9a30c475319e385b2ce79ea5c67fd9ecbbec0
Jan Lippert <lippertsjan@gmail.com>
2017-04-10 19:49:27 +0300

    Added ugly tray icon and minimize to tray

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,12 +1,57 @@
-﻿namespace InteractiveOfficeClient
+﻿using System.IO;
+using System.Reflection;
+using Gdk;
+using GLib;
+
+namespace InteractiveOfficeClient
 {
     using Gtk;
     public class HelloWorld {
+        // The tray Icon
+        private static StatusIcon trayIcon;
+        // The Window
+        private static Gtk.Window window;
+
+        static string workingDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
         static void Main() {
+            // Initialize GTK#
             Application.Init ();
-            MessageDialog Hello = new MessageDialog (null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Hello world!");
-            Hello.SecondaryText="This is an example dialog.";
-            Hello.Run ();
+
+            // Create a Window with title
+            window = new Gtk.Window ("Hello World");
+
+            // Attach to the Delete Event when the window has been closed.
+            window.DeleteEvent += delegate { Application.Quit (); };
+
+            // Creation of the Icon
+            trayIcon = new StatusIcon(new Pixbuf(workingDirectory + "/Resources/app_icon.png"));
+            trayIcon.Visible = true;
+
+            // Show/Hide the window (even from the Panel/Taskbar) when the TrayIcon has been clicked.
+            trayIcon.Activate += delegate { window.Visible = !window.Visible; };
+            // Show a pop up menu when the icon has been right clicked.
+            trayIcon.PopupMenu += OnTrayIconPopup;
+
+            // A Tooltip for the Icon
+            trayIcon.TooltipText = "Hello World Icon";
+
+            // Show the main window and start the application.
+            window.ShowAll ();
+            Application.Run ();
+        }
+
+        // Create the popup menu, on right click.
+        static void OnTrayIconPopup (object o, PopupMenuArgs popupMenuArgs) {
+            Menu popupMenu = new Menu();
+            ImageMenuItem menuItemQuit = new ImageMenuItem ("Quit");
+            Gtk.Image appimg = new Gtk.Image(Stock.Quit, IconSize.Menu);
+            menuItemQuit.Image = appimg;
+            popupMenu.Add(menuItemQuit);
+            // Quit the application when quit has been clicked.
+            menuItemQuit.Activated += delegate { Application.Quit(); };
+            popupMenu.ShowAll();
+            popupMenu.Popup();
         }
     }
 
--- README.md
+++ README.md
@@ -13,9 +13,7 @@ MacOs and Windows, see http://www.mono-project.com/download/
 ## Next Steps
 
  * Add "minimize to tray" to keep the app running when window is closed. Useful links: 
-   * https://www.codeproject.com/Articles/27142/Minimize-to-tray-with-GTK
-   * http://www.jezra.net/blog/minimizeclose_to_system_tray_in_Python_GTK
-   * http://www.c-sharpcorner.com/UploadFile/f9f215/how-to-minimize-your-application-to-system-tray-in-C-Sharp/
+   * http://www.mono-project.com/docs/gui/gtksharp/widgets/notification-icon/
  * Add "start on boot"
    * No research done yet
  * Add the simplified tracking work-flow: ["start working"] -(30min)-> ["start break"] -(10min)-> ["start working"] ... 
