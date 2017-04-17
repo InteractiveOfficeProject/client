2e574ef0e56c470fd1399a401daab7e53ab7382a
Jan Lippert <lippertsjan@gmail.com>
2017-04-17 19:40:21 +0300

    Added DefaultActivities

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -6,6 +6,13 @@
         readonly string Name;
         private readonly int MaximumUsers;
 
+        Activity[] DefaultActivities = new Activity[]
+        {
+            new Activity(-1, "Coffee Break", 0),
+            new Activity(-2, "Walking", 0),
+            new Activity(-3, "Play a Game", 0),
+            new Activity(-4, "Sing a Song", 0)
+        };
 
         public Activity(int activityId, string name, int maximumUsers)
         {
