b356d36f7afc4c504daf688d62e251f4b15b6479
Jan Lippert <lippertsjan@gmail.com>
2017-04-19 15:22:24 +0300

    Extraxted timing functionality

---
+++ ApplicationTimer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace InteractiveOfficeClient
+{
+    public class ApplicationTimer
+    {
+        private readonly MainWindow Context;
+
+        private bool _isWorking;
+
+        public bool IsWorking
+        {
+            get { return _isWorking; }
+            set
+            {
+                _isWorking = value;
+                if (_isWorking)
+                    TimeLeft = INTERVAL_25_MIN_AS_SECONDS;
+                else
+                    TimeLeft = INTERVAL_5_MIN_AS_SECONDS;
+                IsReceivingTicks = true;
+            }
+    }
+
+        private bool IsReceivingTicks = false;
+
+        private TimeSpan TimeSpanTickInterval = TimeSpan.FromSeconds(1);
+
+        #if DEBUG
+        private static readonly int INTERVAL_MINUTE = 1;
+        #else
+        private static readonly int INTERVAL_MINUTE = 60;
+        #endif
+
+        private static readonly int INTERVAL_5_MIN_AS_SECONDS = 5*INTERVAL_MINUTE;
+        private static readonly int INTERVAL_25_MIN_AS_SECONDS = 5*INTERVAL_5_MIN_AS_SECONDS;
+
+        private int TimeLeft = 0;
+
+
+        public ApplicationTimer(MainWindow Context)
+        {
+            this.Context = Context;
+            new System.Threading.Timer(new TimerCallback(OnTimerCallback), "", TimeSpan.Zero, TimeSpanTickInterval);
+        }
+
+        private void OnTimerCallback(object state)
+        {
+            if(!IsReceivingTicks){
+                return;
+            }
+
+            TimeLeft = TimeLeft - 1;
+
+            if (TimeLeft <= 0)
+            {
+                Context.Deiconify();
+                Context.Visible = true;
+                TimeLeft = 0;
+                if (IsWorking)
+                {
+                    Console.WriteLine("Show Activities");
+                    IsReceivingTicks = false;
+                }
+            }
+            Context.TimeLeft = TimeLeft;
+        }
+
+    }
+}
--- MainWindow.cs
+++ MainWindow.cs
@@ -11,23 +11,8 @@ namespace InteractiveOfficeClient
         private readonly Button BtnStartWorking = new Button("Start Working");
         private readonly Button BtnStartBreak = new Button("Start Break");
         private readonly Label LabelTimeLeft = new Label("");
+        private readonly ApplicationTimer _applicationTimer;
 
-
-        private bool IsWorking = false;
-        private bool IsReceivingTicks = false;
-
-        private TimeSpan TimeSpanTickInterval = TimeSpan.FromSeconds(1);
-
-        #if DEBUG
-        private static readonly int INTERVAL_MINUTE = 1;
-        #else 
-        private static readonly int INTERVAL_MINUTE = 60;
-        #endif
-
-        private static readonly int INTERVAL_5_MIN_AS_SECONDS = 5*INTERVAL_MINUTE;
-        private static readonly int INTERVAL_25_MIN_AS_SECONDS = 5*INTERVAL_5_MIN_AS_SECONDS;
-
-        private int TimeLeft = 0;
 
         public MainWindow() : base("Interactive Office Project")
         {
@@ -36,14 +21,27 @@ namespace InteractiveOfficeClient
             Grid.AttachNextTo(BtnStartWorking, LabelTimeLeft, PositionType.Bottom, 1, 1);
             Grid.AttachNextTo(BtnStartBreak, BtnStartWorking, PositionType.Bottom, 1, 1);
 
-            BtnStartWorking.Clicked += delegate {
-                SetWorkingState(true);
-            };
+            BtnStartWorking.Clicked += delegate { SetWorkingState(true); };
             BtnStartBreak.Clicked += delegate { SetWorkingState(false); };
 
-            ShowAll();
+            _applicationTimer = new ApplicationTimer(this);
 
-            new System.Threading.Timer(new TimerCallback(OnTimerCallback), "", TimeSpan.Zero, TimeSpanTickInterval);
+            ShowAll();
+        }
+
+        public int TimeLeft
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    LabelTimeLeft.Text = "";
+                }
+                else
+                {
+                    LabelTimeLeft.Text = $"{value}s";
+                }
+            }
         }
 
         protected override bool OnDeleteEvent(Event evnt)
@@ -53,49 +51,19 @@ namespace InteractiveOfficeClient
             return true;
         }
 
-        private void OnTimerCallback(object state)
-        {
-            if(!IsReceivingTicks){
-                return;
-            }
-
-            TimeLeft = TimeLeft - 1;
-            LabelTimeLeft.Text = $"Time Left: {TimeLeft}s";
-
-            if (TimeLeft <= 0)
-            {
-                Deiconify();
-                Visible = true;
-                TimeLeft = 0;
-                LabelTimeLeft.Text = "";
-                if (IsWorking)
-                {
-                    Console.WriteLine("Show Activities");
-                    IsReceivingTicks = false;
-                }
-            }
-        }
-
         private void SetWorkingState(bool newState)
         {
-            IsWorking = newState;
-
-            if (IsWorking)
-                TimeLeft = INTERVAL_25_MIN_AS_SECONDS;
-            else
-                TimeLeft = INTERVAL_5_MIN_AS_SECONDS;
-
+            _applicationTimer.IsWorking = newState;
             UpdateUi();
             Visible = false;
             Iconify();
-            IsReceivingTicks = true;
         }
 
         private void UpdateUi()
         {
-            BtnStartWorking.Sensitive = !IsWorking;
-            BtnStartBreak.Sensitive = IsWorking;
-}
+            BtnStartWorking.Sensitive = !_applicationTimer.IsWorking;
+            BtnStartBreak.Sensitive = _applicationTimer.IsWorking;
+        }
 
         protected override bool OnVisibilityNotifyEvent(EventVisibility evnt)
         {
@@ -103,6 +71,5 @@ namespace InteractiveOfficeClient
             UpdateUi();
             return baseResult;
         }
-
     }
 }
