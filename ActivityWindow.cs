06ab9266fc41d5af8eb5fd928e0fa94cbebb3f08
Jan Lippert <lippertsjan@gmail.com>
2017-05-02 18:02:56 +0300

    Activity selection like in mockups

--- ActivityWindow.cs
+++ ActivityWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Gtk;
 using InteractiveOfficeClient.Models;
@@ -9,15 +10,16 @@ namespace InteractiveOfficeClient
     {
         private InteractiveOfficeClient _app;
         private readonly Grid _grid = new Grid();
+        private readonly Activity[] _activities;
 
-
+        private readonly HashSet<Activity> _selectedActivites = new HashSet<Activity>();
 
         public ActivityWindow(InteractiveOfficeClient interactiveOfficeClient) : base("Break Time")
         {
             this._app = interactiveOfficeClient;
             Add(_grid);
             ShowLoading(true);
-            var activities = Activity.DefaultActivities;
+            _activities = Activity.DefaultActivities;
 
             int activityRowOffset;
             if (FeatureToggles.FakeTakeBreakNotification)
@@ -33,41 +35,66 @@ namespace InteractiveOfficeClient
             }
 
 
-            for (int i = 0; i < activities.Length; i++)
+            for (int i = 0; i < _activities.Length; i++)
             {
-                var activity = activities[i];
+                var activity = _activities[i];
 
-                Button b = new ActivityButton(activity);
-                b.Clicked += delegate { ActivitySelected(activity); };
+                CheckButton b = new ActivityButton(activity);
+                b.Clicked += delegate { SelectedActivity(activity, b.Active); };
 
                 _grid.Attach(b, 0, activityRowOffset + i, 1, 1);
             }
+
+            Button continueButton = new Button("OK");
+            continueButton.Clicked += delegate { ContinueButtonClicked(); };
+            _grid.Attach(continueButton, 1, activityRowOffset + _activities.Length, 1, 1);
+
             ShowLoading(false);
+        }
+
+        private void SelectedActivity(Activity activity, bool isSelected)
+        {
+            if (isSelected)
+            {
+                Console.WriteLine($"Selected {activity}");
+                _selectedActivites.Add(activity);
+            }
+            else
+            {
+                Console.WriteLine($"De-Selected {activity}");
+                _selectedActivites.Remove(activity);
+            }
         }
 
         private void AddLabel(string text, int row, int col)
         {
-             _grid.Attach(new Label(text), row, col, 1, 1);
+            _grid.Attach(new Label(text), row, col, 1, 1);
         }
 
-        private void ActivitySelected(Activity activity)
+        private void ContinueButtonClicked()
         {
-            Console.WriteLine($"Selected {activity}");
+            Console.WriteLine($"Selected activities:");
+            foreach (var activity in _selectedActivites)
+            {
+                Console.WriteLine($"  {activity}");
+            }
             _app.State = AppState.Break;
             Close();
         }
+
 
         private void ShowLoading(bool isLoading)
         {
             Console.WriteLine($"IsLoading: {isLoading}");
         }
-
     }
 
-    class ActivityButton: Gtk.Button
+    class ActivityButton : Gtk.CheckButton
     {
         public ActivityButton(Activity a)
         {
+            if (FeatureToggles.ShowMissingActivityIcons) Image = new Gtk.Image(Gtk.Stock.MissingImage);
+
             if (a.MaximumUsers > 0)
             {
                 Label = $"{a.Name} ({a.MaximumUsers})";
