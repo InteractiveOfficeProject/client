d8e9e59c51a9009b0e707853438e38e3174f31be
Jan Lippert <lippertsjan@gmail.com>
2017-05-09 12:52:35 +0300

    Better names and documentation for FeatureToggles.cs

--- FeatureToggles.cs
+++ FeatureToggles.cs
@@ -2,9 +2,24 @@
 {
     public class FeatureToggles
     {
+        /// <summary>
+        ///
+        /// </summary>
         public static readonly bool FakeTakeBreakNotification = true;
+        /// <summary>
+        /// Currently, we do not have icons for the activities. The implemented buttons however can show an icons;
+        /// When this feature toggle is enabled, the App shows "Missing image" for these icons. Otherwise, only text
+        /// is loaded.
+        /// </summary>
         public static readonly bool ShowMissingActivityIcons = false;
+        /// <summary>
+        /// Currently, we don't match people up. Therefore, this part of the application is not implemented. This
+        /// feature toggle enables "Fake Matching", i.e. the authors will be shown as potential break partners.
+        /// </summary>
         public static readonly bool ShowFakePeopleSelection = true;
-        public static readonly bool ShowMissingUserIcons = true;
+        /// <summary>
+        ///
+        /// </summary>
+        public static readonly bool LoadUserIcons = true;
     }
 }
